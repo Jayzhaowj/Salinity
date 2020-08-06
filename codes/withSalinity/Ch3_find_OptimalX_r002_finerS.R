418b9f61faf89c2dae0f53c1876017d9d19a5763
YiqingYao <yqyao@ucdavis.edu>
2020-08-06 00:17:33 -0700

    finer inverval for lower discount rate

---
+++ codes/withSalinity/Ch3_find_OptimalX_r002_finerS.R
@@ -0,0 +1,42 @@
+library(snowfall)
+library(nloptr)
+#dir <- "/Users/johnn/Documents/Research/Salinity/"
+dir <- "/soe/wjzhao/project/Salinity/"
+setwd(dir)
+source(paste0(getwd(), "/codes/withSalinity/Salinity_funs_new.R"))
+
+########## Find optimal X ###############
+GW <- seq(8000000, 12000000, 500000)
+XpI <- seq(25000, 75000, 5000)
+XpF <- seq(50000, 150000, 10000)
+Cgw <- seq(500, 5000, 250)
+
+type <- NULL
+hyper_par <- list(x0 = numeric(15), P = rep(0.2, 5), 
+                  xLB = numeric(15), R = 0.02, type = type, Cgw = Cgw)
+
+
+#### set up number of cores
+NofCore <- 11
+
+
+#### set up initial and final table
+initial <- CreateGrid(list(GW, XpI, Cgw))
+init_fin <- CreateGrid(list(GW, XpF, GW, XpI, Cgw))
+#final <- CreateGrid(GW, XpF)
+
+
+#### start parallel
+sfInit(parallel = TRUE, cpus = NofCore, type = "SOCK")
+sfLibrary(nloptr)
+
+#### find solutions
+Init_Fin_Divde <- DivideWork(init_fin, NofCore)
+tmp_time <- proc.time()
+sfExportAll()
+temp_OptimalX <- sfLapply(x = Init_Fin_Divde, fun = function(W) findX(init_fin = W, 
+                                                                      t = 3, hyper_par = hyper_par))
+
+OptimalX_backup <- unlist(temp_OptimalX, recursive = FALSE)
+sfStop()
+save(OptimalX_backup, file = paste0(getwd(), "/results/withSalinity/OptimalX_r002_finerS.RData"))
